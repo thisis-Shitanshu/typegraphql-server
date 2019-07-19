7f4fe1dbceacbbd9fe36e1ac4c8bc73cb5ae3166
namaste-code <shitanshu.19p@gmail.com>
2019-07-20 02:27:08 +0530

    logout feature.

---
+++ src/modules/shared/OkMixin.ts
@@ -0,0 +1,15 @@
+import { 
+    InputType, 
+    Field, 
+    ClassType
+} from 'type-graphql';
+
+export const OkMixin = <T extends ClassType>(BaseClass: T) => {
+    @InputType()
+    class OkInput extends BaseClass {
+        @Field()
+        ok: boolean;
+    }
+
+    return OkInput;
+};
--- src/modules/utils/createConfirmationUrl.ts
+++ src/modules/utils/createConfirmationUrl.ts
@@ -1,11 +1,11 @@
 import {v4} from 'uuid'; 
 import { redis } from '../../redis';
-import { confirmationPrefix } from '../constants/redisPrefixes';
+import { confirmUserPrefix } from '../constants/redisPrefixes';
 
 export const createConfirmationUrl = async (userId: number) => {
     const token = v4();
 
-    await redis.set(confirmationPrefix + token, userId, "ex", 60*60*24);  // one day expiration
+    await redis.set(confirmUserPrefix + token, userId, "ex", 60*60*24);  // one day expiration
 
     return `http://localhost:3000/user/confirm/${token}`;
 };
